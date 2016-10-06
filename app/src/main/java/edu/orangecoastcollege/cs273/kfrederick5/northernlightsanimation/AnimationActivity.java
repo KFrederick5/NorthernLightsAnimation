e264f7ba9159d0327d08586e6add351b75a9e6ac
Keegan Frederick <tefsuncle@hotmail.com>
2016-10-05 23:04:22 -0700

    Worked the animations.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/northernlightsanimation/AnimationActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class AnimationActivity extends AppCompatActivity {
 
     private ImageView lightsImageView;
+    private LinearLayout activity_animation;
 
     private AnimationDrawable frameAnim;
     private Animation rotateAnim;
@@ -23,31 +25,57 @@ public class AnimationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_animation);
 
         lightsImageView = (ImageView) findViewById(R.id.lightsImageView);
+        activity_animation = (LinearLayout) findViewById(R.id.activity_animation);
 
 
 
     }
 
-    public AnimationDrawable frameButton(View view)
+    public void frameButton(View view)
     {
         lightsImageView.setBackgroundResource(R.drawable.frame_anim);
         frameAnim = (AnimationDrawable) lightsImageView.getBackground();
-        frameAnim.start();
+        if(frameAnim.isRunning())
+            frameAnim.stop();
+        else
+            frameAnim.start();
     }
 
-    public Animation rotateButton(View view)
+    public void rotateButton(View view)
     {
-        rotateAnim = (Animation) lightsImageView.getBackground();
+        rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+        if(rotateAnim.hasStarted() && rotateAnim != null) {
+            lightsImageView.clearAnimation();
+            activity_animation.clearAnimation();
+            rotateAnim = null;
+        }
+        else {
+            lightsImageView.startAnimation(rotateAnim);
+            activity_animation.startAnimation(rotateAnim);
+        }
 
     }
 
-    public Animation shakeButton(View view)
+    public void shakeButton(View view)
     {
-
+        shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
+        if(shakeAnim.hasStarted() && rotateAnim != null) {
+            lightsImageView.clearAnimation();
+            shakeAnim = null;
+        }
+        else
+            lightsImageView.startAnimation(shakeAnim);
     }
 
-    public Animation pulseButton(View view)
+    public void pulseButton(View view)
     {
-
+        pulseAnim = AnimationUtils.loadAnimation(this, R.anim.pulse_anim);
+        if(pulseAnim.hasStarted() && pulseAnim != null){
+            lightsImageView.clearAnimation();
+            pulseAnim = null;
+        }
+        else {
+            lightsImageView.startAnimation(pulseAnim);
+        }
     }
 }
